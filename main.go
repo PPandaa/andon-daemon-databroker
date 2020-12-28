41194d1bdd8064a9d9b5b3d7d561e006ce014894
Peter Hu <any99147@gmail.com>
2020-12-28 15:00:12 +0800

    delete startergo

--- main.go
+++ main.go
@@ -1,13 +1,90 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
 	"sync"
+	"time"
+
+	"github.com/bitly/go-simplejson"
+	"github.com/joho/godotenv"
+	"gopkg.in/mgo.v2"
 )
+
+var taipeiTimeZone, utcTimeZone *time.Location
+var eiToken, mongodbURL, mongodbUsername, mongodbPassword, mongodbDatabase, adminUsername, adminPassword, ssoURL string
+
+func initGlobalVar() {
+	taipeiTimeZone, _ = time.LoadLocation("Asia/Taipei")
+	utcTimeZone, _ = time.LoadLocation("UTC")
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Fatalf("Error loading env file")
+	}
+	adminUsername = os.Getenv("ADMIN_USERNAME")
+	adminPassword = os.Getenv("ADMIN_PASSWORD")
+	ssoURL = os.Getenv("SSO_URL")
+	mongodbURL = os.Getenv("MONGODB_URL")
+	mongodbUsername = os.Getenv("MONGODB_USERNAME")
+	mongodbPassword = os.Getenv("MONGODB_PASSWORD")
+	mongodbDatabase = os.Getenv("MONGODB_DATABASE")
+	fmt.Println("MongoDB ->", " URL:", mongodbURL, " Database:", mongodbDatabase)
+}
+
+func refreshEIToken() {
+	for {
+		log.Println("RefreshEIToken")
+		httpClient := &http.Client{}
+		//----------get EIToken => var(eiToken)----------
+		httpRequestBody, _ := json.Marshal(map[string]string{
+			"username": adminUsername,
+			"password": adminPassword,
+		})
+		request, _ := http.NewRequest("POST", ssoURL+"/v4.0/auth/native", bytes.NewBuffer(httpRequestBody))
+		request.Header.Set("accept", "application/json")
+		request.Header.Set("Content-Type", "application/json")
+		response, err := httpClient.Do(request)
+		for err != nil {
+			fmt.Println("Refresh EIToken Fail , Retry!!")
+			response, err = httpClient.Do(request)
+		}
+		m, _ := simplejson.NewFromReader(response.Body)
+		eiToken = "EIToken=" + m.Get("accessToken").MustString()
+		time.Sleep(60 * time.Minute)
+	}
+}
+
+//BrokerStarter ...
+func BrokerStarter() {
+	var nowTime time.Time
+	log.Printf("BrokerStarter Activation")
+	session, _ := mgo.Dial(mongodbURL)
+	db := session.DB(mongodbDatabase)
+	db.Login(mongodbUsername, mongodbPassword)
+	for {
+		nowTime = time.Now().In(taipeiTimeZone)
+		// if nowTime.Minute()%2 == 0 && nowTime.Second() == 0 {
+		// 	DataBroker.TransmitData(eiToken, nowTime, taipeiTimeZone, mongodbURL, mongodbUsername, mongodbPassword, mongodbDatabase)
+		// }
+		// --------- broker.go
+		// fmt.Println("-- TransmitData Start", time.Now().In(taipeiTimeZone))
+		TransmitData(eiToken, db)
+		// fmt.Println("-- TransmitData End", time.Now().In(taipeiTimeZone))
+		time.Sleep(1 * time.Second)
+	}
+}
 
 var wg sync.WaitGroup
 
 func main() {
-	wg.Add(1)
-	go Starter()
+	log.Printf("Main Activation")
+	wg.Add(2)
+	initGlobalVar()
+	go refreshEIToken()
+	go BrokerStarter()
 	wg.Wait()
 }
